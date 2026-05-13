e2963dd71ce7f74aee3aeab4ec827fbdbfb58d72
jamesianfoo <jamesian.foo@gmail.com>
2026-05-13 11:22:19 +1000

    chore: untrack app/build artifacts (already in .gitignore)

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_souspantry_app_SousPantryApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.souspantry.app.SousPantryApp"
-)
-public final class _com_souspantry_app_SousPantryApp {
-}
